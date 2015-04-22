43bdcd220e4b6675d8ee004a0c2201b6c0f33b40
NAN009 <598333549@qq.com>
2015-04-22 22:08:04 +0800

    P63

--- code/main.cpp
+++ code/main.cpp
@@ -5,14 +5,38 @@ using namespace std;
 
 int main()
 {
-	C c1(0), c2(10);
-	for (int i = 0, tmp; i < 10; ++i)
-	{
-		tmp = c1.incr();
-		cout << setw(tmp) << setfill(' ') << tmp << endl;
-		tmp = c2.decr();
-		cout << setw(tmp) << setfill(' ') << tmp << endl;
-	}
+	
+
+	/*cout << sizeof(F) << endl;
+	cout << sizeof(G) << endl;
+	cout << sizeof(H) << endl;
+	cout << sizeof(I) << endl;*/
+	/*cout << sizeof(A1) << endl;
+	cout << sizeof(A2) << endl;
+	cout << sizeof(A3) << endl;
+	cout << sizeof(A4) << endl;
+	cout << sizeof(A5) << endl;
+	cout << sizeof(D) << endl;*/
+	/*char *ss1 = "0123456789";
+	char ss2[] = "0123456789";
+	char ss3[100] = "0123456789";
+	int ss4[100];
+	char q1[] = "abc";
+	char q2[] = "a\n";
+	char *q3 = "a\n";
+	char *str1 = (char *)malloc(100);
+	void *str2 = (void *)malloc(100);
+	cout << sizeof(ss1) << endl;
+	cout << sizeof(ss2) << endl;
+	cout << sizeof(ss3) << endl;
+	cout << sizeof(ss4) << endl;
+	cout << sizeof(q1) << endl;
+	cout << sizeof(q2) << endl;
+	cout << sizeof(q3) << endl;
+	cout << sizeof(A) << endl;
+	cout << sizeof(B) << endl;
+	cout << sizeof(str1) << endl;
+	cout << sizeof(str2) << endl;*/
 	/*//用一个表达式判断x是否是2的n次方，不可用循环语句
 	!x&(x - 1);	
 	unsigned char a = 0xA5;
