4cb8b5046538ef6b3386dfa4453b64f76935b152
NAN009 <598333549@qq.com>
2015-08-19 22:08:42 +0800

    hashtable

--- code/剑指offer.h
+++ code/剑指offer.h
@@ -651,3 +651,24 @@ vector<int> FindNumbersWithSum(vector<int> array, int sum)
 		return vector<int>();
 	return vec;
 }
+//
+char FirstNotRepeatingChar(char* pString)
+{
+	if (pString == nullptr)
+		return '\0';
+	const int tableSize = 256;
+	unsigned int hashTable[tableSize];
+	for (unsigned int i = 0; i < tableSize; ++i)
+		hashTable[i] = 0;
+	char *pHashKey = pString;
+	while (*pHashKey != '\0')
+		hashTable[*(pHashKey++)]++;
+	pHashKey = pString;
+	while (*pHashKey != '\0')
+	{
+		if (hashTable[*pHashKey] == 1)
+			return *pHashKey;
+		pHashKey++;
+	}
+	return '\0';
+}
